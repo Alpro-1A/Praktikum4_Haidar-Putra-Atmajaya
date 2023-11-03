c921089247d5027f9767fd4b613a7dff2797f367
haidaratmjya <147780250+haidaratmjya@users.noreply.github.com>
2023-11-04 06:35:57 +0700

    Add files via upload

---
+++ tugas2.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+
+using namespace std;
+int main(){
+	cout << "MEMBUAT PIRAMIDA BINTANG"<<endl;
+	int n;
+
+	cout << "    Masukkan tinggi piramida: "; cin >> n;
+
+	char currentChar = '*';
+	for (int i = 0; i <= n; i++){
+		for (int j = n; j > i; j--){
+			cout << " ";
+		}
+	for (int k = 1; k <= i; k++){
+		cout << currentChar << " ";
+		}
+
+		cout << endl;
+		}
+
+return 0;
+}
